22d6d2c14d1a4ed72400b2ddf4df4ec9280efb55
Sheldon Cohen <sheldon@sheldonrcohen.com>
2020-11-20 10:37:36 -0500

    Initial Commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('qr app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to qr!');
   });
 
   afterEach(async () => {
---
+++ src/app/entity/qr-objects.ts
@@ -0,0 +1,11 @@
+export class QR {
+
+    text:           string;
+    imageBase64:    string;
+ 
+    constructor(text: string, imageBase64: string) {
+        this.imageBase64 = imageBase64;
+        this.text = text;
+    }
+ 
+ }
